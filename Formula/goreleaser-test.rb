d2e85103eff364de1f72a94628ff6d60be4c4355
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-24 12:03:58 +0200

    goreleaser-test version v0.2.0

--- Formula/goreleaser-test.rb
+++ Formula/goreleaser-test.rb
@@ -3,11 +3,11 @@ class GoreleaserTest < Formula
   homepage "https://example.com/"
 
   
-    url "https://github.com/sklevenz/goreleaser-test/releases/download/v0.1.0/goreleaser-test_0.1.0_macOS_64-bit.tar.gz"
+    url "https://github.com/sklevenz/goreleaser-test/releases/download/v0.2.0/goreleaser-test_0.2.0_macOS_64-bit.tar.gz"
   
 
-  version "0.1.0"
-  sha256 "cd794f311d76ebd56970df5b3e6e937a4de2be54fce09ea006dcf92ced356463"
+  version "0.2.0"
+  sha256 "d2e32fba6809a312dc854c39182f12d03d032f78749ecde537b41351073e10eb"
 
   def install
     bin.install "goreleaser-test"
