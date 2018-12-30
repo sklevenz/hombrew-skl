9030073403da10cb968961136b19be5d74fcf1ac
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-30 12:01:05 +0100

    Brew formula update for k8s-serv version 0.6.1

--- Formula/k8s-serv.rb
+++ Formula/k8s-serv.rb
@@ -1,9 +1,9 @@
 class K8sServ < Formula
   desc "Learning github, travis, go, k8s, docker ..."
   homepage "https://github.com/sklevenz/k8s-serv"
-  url "https://github.com/sklevenz/k8s-serv/releases/download/0.6.0/k8s-serv_0.6.0_Darwin_x86_64.tar.gz"
-  version "0.6.0"
-  sha256 "6826244633cf9c104ef86fcbc899ef09e9ceb75e3b1a8a39e2a2700f7e13f631"
+  url "https://github.com/sklevenz/k8s-serv/releases/download/0.6.1/k8s-serv_0.6.1_Darwin_x86_64.tar.gz"
+  version "0.6.1"
+  sha256 "3d167561c54cece4c15dc44968403b8324e23aea00687b7294bf656ec02b2648"
 
   def install
     bin.install "k8s-serv"
