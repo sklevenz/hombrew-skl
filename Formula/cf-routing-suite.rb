11929c4d12cce68d7a498a8277ffe56bc1686319
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-03 18:33:31 +0100

    Brew formula update for cf-routing-suite version 0.0.0

--- Formula/cf-routing-suite.rb
+++ Formula/cf-routing-suite.rb
@@ -3,7 +3,7 @@ class CfRoutingSuite < Formula
   homepage "https://github.com/sklevenz/cf-routing-suite/"
   url "https://github.com/sklevenz/cf-routing-suite/releases/download/0.0.0/cf-routing-suite_0.0.0_macOS_64-bit.tar.gz"
   version "0.0.0"
-  sha256 "4f338b6fcbbedc0300f75679abb7f62ff915d5ce4e76b91b5876141041608d02"
+  sha256 "8d97617ae3904ca695a1f91fa60d36742db409b827950a3956fb0c8757ad86fc"
 
   def install
     bin.install "cf-routing-suite/cfrs-client"
