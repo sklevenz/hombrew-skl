a95a11a7e5e2d1ba28948383ca7b218f7758954a
Stephan Klevenz <stephan.klevenz@sap.com>
2023-04-03 16:44:32 +0200

    Update btp-cli.rb

--- Formula/btp-cli.rb
+++ Formula/btp-cli.rb
@@ -3,7 +3,7 @@ class BtpCli < Formula
   homepage "https://tools.hana.ondemand.com/#cloud-btpcli"
   url "https://tools.hana.ondemand.com/additional/btp-cli-darwin-arm64-2.38.0.tar.gz"
   version "2.38.0"
-  sha256 "b89093c1cb7ba37744da38bf2d4d34157e3cdc02"
+  sha1 "b89093c1cb7ba37744da38bf2d4d34157e3cdc02"
 
   def install
     bin.install "btp"
