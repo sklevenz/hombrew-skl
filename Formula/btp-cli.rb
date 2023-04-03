1c9033e01cd8cc6378dff9f640f6d54d8d626b9a
Stephan Klevenz <stephan.klevenz@sap.com>
2023-04-03 16:34:22 +0200

    Create btp-cli.rb

---
+++ Formula/btp-cli.rb
@@ -0,0 +1,20 @@
+class BTPCli < Formula
+  desc "SAP BTP Platfom CLI"
+  homepage "https://tools.hana.ondemand.com/#cloud-btpcli"
+  url "https://tools.hana.ondemand.com/additional/btp-cli-darwin-arm64-2.38.0.tar.gz"
+  version "2.38.0"
+  sha256 "b89093c1cb7ba37744da38bf2d4d34157e3cdc02"
+
+  def install
+    bin.install "btp"
+  end
+
+  def caveats; <<~EOS
+    How to use this binary
+  EOS
+  end
+
+  test do
+    system "#{bin}/btp"
+  end
+end
